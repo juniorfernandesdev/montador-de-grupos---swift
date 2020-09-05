0f6e7a91bfab06c225742e120f3e6aec450dead0
bryan barreto <bryan.barreto@icloud.com>
2020-09-05 08:52:48 -0300

    Initial Commit

---
+++ sorteador de grupos/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  sorteador de grupos
+//
+//  Created by Bryan Barreto on 05/09/20.
+//  Copyright © 2020 Bryan Barreto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
